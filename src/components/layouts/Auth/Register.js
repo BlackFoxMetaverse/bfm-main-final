418527afc6eb4187288287a3f39fd3afd23f2edf
Ritik Bhushan <ritikbhushanmain@gmail.com>
2024-01-17 13:29:21 +0530

    changes

--- src/components/layouts/Auth/Register.js
+++ src/components/layouts/Auth/Register.js
@@ -1,184 +1,184 @@
-"use client";
+// "use client";
 
-import React, { useState } from "react";
-import loginplaceholder from "../../../../public/login.svg";
-import Image from "next/image";
-import { FaUserAlt } from "react-icons/fa";
-import { useRouter } from "next/navigation";
-import axios from "@/utils/axios";
-import s3ImageUplaod from "@/utils/imageUploader";
+// import React, { useState } from "react";
+// import loginplaceholder from "../../../../public/login.svg";
+// import Image from "next/image";
+// import { FaUserAlt } from "react-icons/fa";
+// import { useRouter } from "next/navigation";
+// import axios from "@/utils/axios";
+// import s3ImageUplaod from "@/utils/imageUploader";
 
-const Register = () => {
-  const [image, setImage] = useState(null);
-  const [imageFile, setImageFile] = useState(null);
-  const [name, setName] = useState("");
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [profession, setProfession] = useState("");
-  const router = useRouter();
+// const Register = () => {
+//   const [image, setImage] = useState(null);
+//   const [imageFile, setImageFile] = useState(null);
+//   const [name, setName] = useState("");
+//   const [username, setUsername] = useState("");
+//   const [email, setEmail] = useState("");
+//   const [profession, setProfession] = useState("");
+//   const router = useRouter();
 
-  const handleImageChange = (e) => {
-    const file = e.target.files[0];
-    if (file) {
-      setImageFile(file);
-      const imageUrl = URL.createObjectURL(file);
-      setImage(imageUrl);
-    }
-  };
+//   const handleImageChange = (e) => {
+//     const file = e.target.files[0];
+//     if (file) {
+//       setImageFile(file);
+//       const imageUrl = URL.createObjectURL(file);
+//       setImage(imageUrl);
+//     }
+//   };
 
-  async function handleRegister() {
-    try {
-      const userDetails = JSON.parse(sessionStorage.getItem("userDetails"));
-      const token = localStorage.getItem("bfm-user-token");
+//   async function handleRegister() {
+//     try {
+//       const userDetails = JSON.parse(sessionStorage.getItem("userDetails"));
+//       const token = localStorage.getItem("bfm-user-token");
 
-      // Await the completion of the image upload
-      const uploadedImageFileName = await s3ImageUplaod(imageFile);
+//       // Await the completion of the image upload
+//       const uploadedImageFileName = await s3ImageUplaod(imageFile);
 
-      const res = await axios.post(
-        "/auth/register",
-        {
-          uid: userDetails.uid,
-          image: uploadedImageFileName, // Use the uploaded image file name
-          name: name,
-          userName: username,
-          email: email,
-          phone_number: userDetails.phone_number,
-          profession: profession,
-        },
-        {
-          headers: {
-            idtoken: token,
-          },
-        }
-      );
+//       const res = await axios.post(
+//         "/auth/register",
+//         {
+//           uid: userDetails.uid,
+//           image: uploadedImageFileName, // Use the uploaded image file name
+//           name: name,
+//           userName: username,
+//           email: email,
+//           phone_number: userDetails.phone_number,
+//           profession: profession,
+//         },
+//         {
+//           headers: {
+//             idtoken: token,
+//           },
+//         }
+//       );
 
-      return res;
-    } catch (error) {
-      console.log("register-axios-error: ", error);
-    }
-  }
+//       return res;
+//     } catch (error) {
+//       console.log("register-axios-error: ", error);
+//     }
+//   }
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    handleRegister()
-      .then((res) => {
-        console.log("register-res: ", res);
-        router.push("/profile/editProfile");
-      })
-      .catch((e) => console.log("error: ", e));
-  };
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     handleRegister()
+//       .then((res) => {
+//         console.log("register-res: ", res);
+//         router.push("/profile/editProfile");
+//       })
+//       .catch((e) => console.log("error: ", e));
+//   };
 
-  return (
-    <main className="flex max-w-[1512px] max-h-[982px] h-screen justify-center items-center lg:px-[268px] lg:py-[107px] overflow-hidden">
-      <div className="flex max-w-[976px] max-h-[768px] items-center justify-between gap-[148px] shrink-0">
-        <div className="flex-[1_0_0] self-stretch lg:flex hidden justify-center items-center overflow-hidden w-full aspect-[9/16] shrink-0">
-          <Image
-            src={loginplaceholder}
-            alt=""
-            className="w-full h-full object-cover shrink-0"
-          />
-        </div>
-        <form
-          onSubmit={(e) => handleSubmit(e)}
-          className="flex flex-col items-start gap-[23px] px-5 self-stretch"
-        >
-          <div className="flex items-center gap-5">
-            <div className="flex justify-center items-center gap-[13.451px] rounded-[13.451px]">
-              {image ? (
-                <label htmlFor="imageInput" className="w-full h-full">
-                  <img
-                    src={image}
-                    alt=""
-                    className="flex w-[94px] aspect-square items-start rounded-[102px] border-4 border-black/50"
-                  />
-                  <input
-                    type="file"
-                    id="imageInput"
-                    name="imageInput"
-                    className="hidden"
-                    onChange={handleImageChange}
-                  />
-                </label>
-              ) : (
-                <label htmlFor="imageInput" className="w-full h-full">
-                  <FaUserAlt className="text-8xl flex aspect-square items-start rounded-[102px] border-4 border-black/50" />
-                  <input
-                    type="file"
-                    id="imageInput"
-                    name="imageInput"
-                    className="hidden"
-                    required={false}
-                    onChange={handleImageChange}
-                  />
-                </label>
-              )}
-            </div>
-            <button className="flex justify-center bg-[#4461F2] items-center gap-2.5 px-[21px] py-2.5 rounded-lg text-white text-xl not-italic font-semibold leading-[normal]">
-              Upload Profile
-            </button>
-          </div>
-          <div className="flex flex-col items-start gap-2 self-stretch">
-            <input
-              type="text"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              required
-              placeholder="Name"
-              className="flex w-[344px] items-center gap-1 px-5 py-4 focus:outline-none rounded-xl bg-black/20 text-[color:var(--mono-90,#18181B)] text-base not-italic font-medium leading-6"
-            />
-            <input
-              type="text"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              required
-              placeholder="Username"
-              className="flex w-[344px] items-center gap-1 px-5 py-4 focus:outline-none rounded-xl bg-black/20 text-[color:var(--mono-90,#18181B)] text-base not-italic font-medium leading-6"
-            />
-            <input
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-              placeholder="Email Address"
-              className="flex w-[344px] items-center gap-1 px-5 py-4 focus:outline-none rounded-xl bg-black/20 text-[color:var(--mono-90,#18181B)] text-base not-italic font-medium leading-6"
-            />
-            <select
-              name=""
-              id=""
-              required
-              className="flex w-[344px] items-center gap-1 px-5 py-4 focus:outline-none rounded-xl bg-black/20 text-[color:var(--mono-90,#18181B)] text-base not-italic font-medium leading-6"
-              onChange={(e) => setProfession(e.target.value)}
-            >
-              <option className="bg-black/20 px-5 py-4" value="Profession">
-                Profession
-              </option>
-              <option
-                className="bg-black/20 px-5 py-4"
-                value="Frontend Developer"
-              >
-                Frontend Developer
-              </option>
-              <option
-                className="bg-black/20 px-5 py-4"
-                value="Backend Developer"
-              >
-                Backend Developer
-              </option>
-              <option className="bg-black/20 px-5 py-4" value="Human Resources">
-                Human Resources
-              </option>
-            </select>
-          </div>
-          <button
-            type="submit"
-            className="flex bg-[#0858F7] justify-center items-center gap-2 self-stretch px-5 py-4 rounded-xl text-[color:var(--mono-0,#FFF)] text-center text-base not-italic font-bold leading-6"
-          >
-            View Profile
-          </button>
-        </form>
-      </div>
-    </main>
-  );
-};
+//   return (
+//     <main className="flex max-w-[1512px] max-h-[982px] h-screen justify-center items-center lg:px-[268px] lg:py-[107px] overflow-hidden">
+//       <div className="flex max-w-[976px] max-h-[768px] items-center justify-between gap-[148px] shrink-0">
+//         <div className="flex-[1_0_0] self-stretch lg:flex hidden justify-center items-center overflow-hidden w-full aspect-[9/16] shrink-0">
+//           <Image
+//             src={loginplaceholder}
+//             alt=""
+//             className="w-full h-full object-cover shrink-0"
+//           />
+//         </div>
+//         <form
+//           onSubmit={(e) => handleSubmit(e)}
+//           className="flex flex-col items-start gap-[23px] px-5 self-stretch"
+//         >
+//           <div className="flex items-center gap-5">
+//             <div className="flex justify-center items-center gap-[13.451px] rounded-[13.451px]">
+//               {image ? (
+//                 <label htmlFor="imageInput" className="w-full h-full">
+//                   <img
+//                     src={image}
+//                     alt=""
+//                     className="flex w-[94px] aspect-square items-start rounded-[102px] border-4 border-black/50"
+//                   />
+//                   <input
+//                     type="file"
+//                     id="imageInput"
+//                     name="imageInput"
+//                     className="hidden"
+//                     onChange={handleImageChange}
+//                   />
+//                 </label>
+//               ) : (
+//                 <label htmlFor="imageInput" className="w-full h-full">
+//                   <FaUserAlt className="text-8xl flex aspect-square items-start rounded-[102px] border-4 border-black/50" />
+//                   <input
+//                     type="file"
+//                     id="imageInput"
+//                     name="imageInput"
+//                     className="hidden"
+//                     required={false}
+//                     onChange={handleImageChange}
+//                   />
+//                 </label>
+//               )}
+//             </div>
+//             <button className="flex justify-center bg-[#4461F2] items-center gap-2.5 px-[21px] py-2.5 rounded-lg text-white text-xl not-italic font-semibold leading-[normal]">
+//               Upload Profile
+//             </button>
+//           </div>
+//           <div className="flex flex-col items-start gap-2 self-stretch">
+//             <input
+//               type="text"
+//               value={name}
+//               onChange={(e) => setName(e.target.value)}
+//               required
+//               placeholder="Name"
+//               className="flex w-[344px] items-center gap-1 px-5 py-4 focus:outline-none rounded-xl bg-black/20 text-[color:var(--mono-90,#18181B)] text-base not-italic font-medium leading-6"
+//             />
+//             <input
+//               type="text"
+//               value={username}
+//               onChange={(e) => setUsername(e.target.value)}
+//               required
+//               placeholder="Username"
+//               className="flex w-[344px] items-center gap-1 px-5 py-4 focus:outline-none rounded-xl bg-black/20 text-[color:var(--mono-90,#18181B)] text-base not-italic font-medium leading-6"
+//             />
+//             <input
+//               type="email"
+//               value={email}
+//               onChange={(e) => setEmail(e.target.value)}
+//               required
+//               placeholder="Email Address"
+//               className="flex w-[344px] items-center gap-1 px-5 py-4 focus:outline-none rounded-xl bg-black/20 text-[color:var(--mono-90,#18181B)] text-base not-italic font-medium leading-6"
+//             />
+//             <select
+//               name=""
+//               id=""
+//               required
+//               className="flex w-[344px] items-center gap-1 px-5 py-4 focus:outline-none rounded-xl bg-black/20 text-[color:var(--mono-90,#18181B)] text-base not-italic font-medium leading-6"
+//               onChange={(e) => setProfession(e.target.value)}
+//             >
+//               <option className="bg-black/20 px-5 py-4" value="Profession">
+//                 Profession
+//               </option>
+//               <option
+//                 className="bg-black/20 px-5 py-4"
+//                 value="Frontend Developer"
+//               >
+//                 Frontend Developer
+//               </option>
+//               <option
+//                 className="bg-black/20 px-5 py-4"
+//                 value="Backend Developer"
+//               >
+//                 Backend Developer
+//               </option>
+//               <option className="bg-black/20 px-5 py-4" value="Human Resources">
+//                 Human Resources
+//               </option>
+//             </select>
+//           </div>
+//           <button
+//             type="submit"
+//             className="flex bg-[#0858F7] justify-center items-center gap-2 self-stretch px-5 py-4 rounded-xl text-[color:var(--mono-0,#FFF)] text-center text-base not-italic font-bold leading-6"
+//           >
+//             View Profile
+//           </button>
+//         </form>
+//       </div>
+//     </main>
+//   );
+// };
 
-export default Register;
+// export default Register;
--- src/components/layouts/Header/Header.js
+++ src/components/layouts/Header/Header.js
@@ -17,48 +17,26 @@ const Header = () => {
       className={`flex flex-col justify-end items-center lg:pt-[57px] pt-[38.5px] pb-[1.113px] px-0 w-full`}
     >
       <div className="mx-auto w-11/12 flex items-center justify-between">
-        <div className="flex items-center lg:gap-[25px] gap-0.5">
+        <div className="flex items-center lg:gap-[65px] gap-0.5">
           <Image
             src={logo}
             alt=""
-            className="w-[52.443px] h-[16.492px] lg:w-[77.515px] lg:h-[24.376px]"
+            className="w-[52.443px] h-[16.492px] lg:w-[87.515px] lg:h-[34.376px]"
           />
-          <div className="flex justify-center items-center  focus:outline-none lg:gap-[8.04px] gap-[5.44px] p-[5.439px] lg:p-[8.04px]">
-            <HiLocationMarker className="lg:text-2xl text-sm text-[#4461F2]" />
-            <select
-              name=""
-              id=""
-              className="text-black lg:text-[20.904px] text-[9.999px] not-italic font-medium leading-[17.697px]"
-            >
-              <option value="New Delhi">New Delhi, India</option>
-              <option value="Jaipur">Jaipur, India</option>
-            </select>
+          <div className="flex justify-center items-center rounded-full text-xs focus:outline-none text-[#784DC7] bg-[#E9DFFC] lg:gap-[2.04px] gap-[5.44px] p-[5.439px] lg:px-[8px]">
+            <HiLocationMarker className="lg:text-xl text-xs text-[#784DC7]" />
+            New Delhi,India
           </div>
         </div>
-        <form
-          action="search"
-          className="max-w-[523px] w-full bg-black/20 py-3 px-5 lg:flex hidden justify-center items-center shrink-0 rounded-[37px]"
-        >
-          <label
-            htmlFor="tags"
-            className="w-full flex justify-center items-center gap-5"
-          >
-            <BsSearch className="text-xl" />
-            <input
-              type="text"
-              name="tags"
-              id="tags"
-              className="w-full bg-transparent text-[15.969px] not-italic font-normal leading-[normal] focus:outline-none"
-              placeholder="Search Tags"
-            />
-          </label>
-          <button type="submit" className="hidden"></button>
-        </form>
+
         <div className="flex items-center h-full lg:gap-[30px] gap-[11px]">
+          <buttom className=" text-[color:var(--Foundation-Green-green-400,#58975B)] text-xl not-italic font-semibold leading-[100%] tracking-[-1px]">
+            Become a Seller
+          </buttom>
           <button>
             <LuBellDot className="lg:text-3xl text-2xl" />
           </button>
-          <Link
+          {/* <Link
             href={"/auth/register"}
             className="flex bg-[#282828] h-full items-center gap-2.5 lg:px-[23px] lg:py-[7px] px-[11.754px] py-[3.577px] rounded-[34px] text-white lg:text-xl text-[10.22px] not-italic font-normal leading-[normal]"
           >
@@ -69,7 +47,7 @@ const Header = () => {
             className="lg:flex hidden h-full items-center gap-2.5 px-[23px] py-[7px] rounded-[34px] text-black text-xl not-italic font-normal leading-[normal]"
           >
             Log In
-          </Link>
+          </Link> */}
         </div>
       </div>
     </main>
