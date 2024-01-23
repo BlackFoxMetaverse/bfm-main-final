5c91181fa6d3a7677d03cb2b1b17a537de48d2f3
Satyam Kaurav <satyam_031@outlook.com>
2024-01-23 12:32:49 +0530

    update axios utils

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://3.110.249.51/api",
+  baseURL: "https://form.blackfoxmetaverse.io/api",
   // baseURL: "http://localhost:4000",
 });
 
