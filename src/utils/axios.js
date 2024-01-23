f5d11e0cf42de5297cd83e64cb5a97041e0539ca
Satyam Kaurav <satyam_031@outlook.com>
2024-01-23 14:26:05 +0530

    /user to base url

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://form.blackfoxmetaverse.io/api",
-  // baseURL: "http://localhost:4000",
+  baseURL: "https://form.blackfoxmetaverse.io/api/user",
+  // baseURL: "http://localhost:4000/user",
 });
 
 export default instance;
