e6e2c4a3471020e7c24864b4fb65cf84b8131744
Ritik Bhushan <ritikbhushanmain@gmail.com>
2024-02-08 12:22:33 +0530

    folder

---
+++ src/app/(main)/client/username/[username]/[id]/page.jsx
@@ -0,0 +1,12 @@
+import SellerDetails from "@/components/client/layout/SellerDetails/SellerDetails";
+import React from "react";
+
+const page = ({ params }) => {
+  return (
+    <div>
+      <SellerDetails />
+    </div>
+  );
+};
+
+export default page;
