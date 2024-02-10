0c272fcda487e341aaf16c16f72ea0db611b4ebd
Raunak Pandey <praunak82@gmail.com>
2024-02-10 13:08:20 +0530

    seller structure

---
+++ src/app/(seller)/layout.js
@@ -0,0 +1,23 @@
+import { Inter } from "next/font/google";
+import Footer from "@/components/layouts/Footer/Footer";
+import Header from "@/components/layouts/Header/Header";
+import ".././globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Black Fox Metaverse | Seller",
+  description: "BFM Location service",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body suppressHydrationWarning className={inter.className}>
+        <Header isSeller />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
