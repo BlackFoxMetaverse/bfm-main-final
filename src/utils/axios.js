7b9b72d7f54e1de6d65d485edd7dfc7a987646f0
Raunak Pandey <praunak82@gmail.com>
2024-03-01 19:51:34 +0530

    new url

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -4,7 +4,8 @@ const instance = axios.create({
   // baseURL: "https://form.blackfoxmetaverse.io/api",
   // baseURL: "http://localhost:4000",
   // baseURL: "http://3.110.249.51/api",
-  baseURL: "http://43.205.14.24/api",
+  // baseURL: "http://43.205.14.24/api",
+  baseURL: "https://api.blackfoxmetaverse.io",
   // baseURL: "https://boar-trusting-urgently.ngrok-free.app",
 });
 
