f20d19fb71b8d2ce5afe2fa7eade186ba38e5b02
satyam kaurav <satyam_031@outlook.com>
2024-03-04 15:36:02 +0530

    dfsa

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,12 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  // baseURL: "https://form.blackfoxmetaverse.io/api",
-  // baseURL: "http://localhost:4000",
-  // baseURL: "http://3.110.249.51/api",
-  // baseURL: "http://43.205.14.24/api",
   baseURL: "https://api.blackfoxmetaverse.io",
-  // baseURL: "https://boar-trusting-urgently.ngrok-free.app",
 });
 
 export default instance;
